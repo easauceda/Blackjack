9ba0d4f372873181817abe52c06996e72f2dcafe
easauceda <easauceda@gmail.com>
2016-05-05 16:16:28 -0700

    layout created and images imported

--- app/src/main/java/com/cs437/cswithandroid/blackjack/MainActivity.java
+++ app/src/main/java/com/cs437/cswithandroid/blackjack/MainActivity.java
@@ -2,6 +2,7 @@ package com.cs437.cswithandroid.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView test = (ImageView) findViewById(R.id.dealerCard);
+        test.setImageResource(R.drawable.card1);
+        ImageView test2 = (ImageView) findViewById(R.id.playerCard);
+        test2.setImageResource(R.drawable.card2);
     }
 }
