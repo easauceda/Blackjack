6ce15250dcb0d0caec2b2c4ac629e04a3677982e
easauceda <easauceda@gmail.com>
2016-05-05 21:32:49 -0700

    Added Start Activity

--- app/src/main/java/com/cs437/cswithandroid/blackjack/MainActivity.java
+++ app/src/main/java/com/cs437/cswithandroid/blackjack/MainActivity.java
@@ -3,6 +3,8 @@ package com.cs437.cswithandroid.blackjack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         final Button start = (Button) findViewById(R.id.start);
@@ -59,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 hit.setEnabled(false);
+                hold.setEnabled(false);
                 while (dealerScore < 18){
                     dealerDraw();
                     checkStatus();
@@ -146,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
             score = (TextView) findViewById(R.id.dealerScore);
         }
 
-        score.setText("Score: " + i);
+        score.setText(turn + " score: " + i);
+
     }
 }
---
+++ app/src/main/java/com/cs437/cswithandroid/blackjack/StartActivity.java
@@ -0,0 +1,34 @@
+package com.cs437.cswithandroid.blackjack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start);
+        Button play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame(v);
+            }
+        });
+    }
+
+    public void startGame(View v) {
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+        finish();
+    }
+}
